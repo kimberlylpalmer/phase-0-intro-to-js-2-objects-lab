96565f8dfea1375a38219ea209a6dc8f8f477991
Kimberly L Palmer <kimberlylpalmer@gmail.com>
2023-09-01 09:59:18 -0700

    Complete Object Lab

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution in this file!
+const employee = {
+    name: "Kimberly Palmer",
+    streedAddres: "333 Bent Twig Ave",
+}
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newEmployee = {...employee};
+
+    newEmployee[key] = value;
+
+    return newEmployee
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee
+}
+
+function  deleteFromEmployeeByKey(employee, key){
+    const newEmployee = {...employee};
+
+    delete newEmployee[key];
+
+    // return employee
+    return newEmployee;
+        // delete employee
+    }
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key]
+    return employee
+}
+
