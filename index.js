67d74c69467194737b8cbab79dae2f74d10b4ca9
Kimberly Palmer <kimberlylpalmer@gmail.com>
2023-11-01 20:40:29 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,4 +1,39 @@
 // Write your solution in this file!
+
+const employee = {
+    name: "Kimberly Palmer",
+    streetAddress: "333 Bent Twig Avenue Camarillo CA 93012",
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployee = {...employee};
+    newEmployee[key] = value;
+    return newEmployee;
+}
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value
+    return employee
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = {...employee}
+    delete newEmployee[key]
+    return newEmployee
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee
+}
+
+
+
+
+
+
+/*   ~~~~~~~~~~~~~~~~~~~~~~~~~~~Previous Code~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const employee = {
     name: "Kimberly Palmer",
     streedAddres: "333 Bent Twig Ave",
@@ -31,3 +66,4 @@ function destructivelyDeleteFromEmployeeByKey(employee, key) {
     return employee
 }
 
+*/
